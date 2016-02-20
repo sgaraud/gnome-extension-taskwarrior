788a8ea0171950f662c73678119503890b75d481
sylvain garaud <garaud@gmail.com>
2016-02-20 16:00:35 +0100

    exlude .git and .idea folders

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ echo "Compile schema"
 glib-compile-schemas ./schemas/
 
 echo "Create zip archive"
-zip -r taskwarrior-integration@sgaraud.github.com.zip . -x *.git*
+zip -r taskwarrior-integration@sgaraud.github.com.zip . -x \*.git* \*.idea*
 
 echo "Done."
