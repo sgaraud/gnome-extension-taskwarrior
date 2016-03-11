f0ba4e3cbf3152e26e8e6570dc78d7e52a9cdb53
sylvain garaud <garaud@gmail.com>
2016-03-12 00:30:24 +0100

    clean-up

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ echo "Compile schema"
 glib-compile-schemas ./schemas/
 
 echo "Create zip archive"
+rm taskwarrior-integration@sgaraud.github.com.zip
 zip -r taskwarrior-integration@sgaraud.github.com.zip . -x \*.git* \*.idea*
-
 echo "Done."
